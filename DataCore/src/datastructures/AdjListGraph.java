6e2ecdbb1f2bb96294656b6572b3781fa36aa4de
Allan Minn <allanminn@yahoo.com>
2016-07-24 01:26:40 -0700

    Added implementation of AdjListGraph.

--- DataCore/src/datastructures/AdjListGraph.java
+++ DataCore/src/datastructures/AdjListGraph.java
@@ -3,27 +3,36 @@
  */
 package datastructures;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * @author Vaygr
- *
+ * Adjacency List implementation of undirected Graph
  */
-public class AdjacencyListGraph extends Graph {
+public class AdjListGraph extends Graph {
 
+	private HashMap<Vertex, Set<Edge>> edgeList;
 	/**
-	 * 
+	 * Default constructor
 	 */
-	public AdjacencyListGraph() {
-		// TODO Auto-generated constructor stub
+	public AdjListGraph() {
+		super();
+		edgeList = new HashMap<Vertex, Set<Edge>>();
 	}
-
+	
 	/* (non-Javadoc)
-	 * @see datastructures.Graph#implementAddVertex(datastructures.Vertex)
+	 * @see datastructures.Graph#implementAddEdge(datastructures.Edge)
 	 */
 	@Override
 	protected boolean implementAddVertex(Vertex v) {
-		// TODO Auto-generated method stub
+		if ( !edgeList.containsKey(v)){
+			edgeList.put(v, new HashSet<Edge>());
+			return true;
+		}
 		return false;
 	}
 
@@ -32,7 +41,15 @@ public class AdjacencyListGraph extends Graph {
 	 */
 	@Override
 	protected boolean implementAddEdge(Edge e) {
-		// TODO Auto-generated method stub
+		// If the starting vertex is already in the graph find it
+		// And get the list of edges.
+		Set<Edge> edges = edgeList.get(e.getStartVertex());
+		if ( edges != null ){
+			if ( !edges.contains(e)){
+				edges.add(e);
+				return true;
+			}
+		}
 		return false;
 	}
 
@@ -41,8 +58,14 @@ public class AdjacencyListGraph extends Graph {
 	 */
 	@Override
 	protected List<Vertex> implementGetNextNeighbors(Vertex v) {
-		// TODO Auto-generated method stub
-		return null;
+		// Find the vertex in the map and return outgoing edges.
+		List<Vertex> vertList = new ArrayList<Vertex>();
+		if ( edgeList.containsKey(v)){
+			Set<Edge> edges = edgeList.get(v);
+			for ( Edge edge : edges ){
+				vertList.add(edge.getOtherVertex(v));
+			}
+		}
+		return vertList;
 	}
-
 }
