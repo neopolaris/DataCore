920ab2f968c1ac8d7c64bf90500988fcb77721fc
Allan Minn <allanminn@yahoo.com>
2016-07-24 00:49:06 -0700

    Added Implementation of Vertex class.

--- DataCore/src/datastructures/Vertex.java
+++ DataCore/src/datastructures/Vertex.java
@@ -5,8 +5,41 @@ package datastructures;
 
 /**
  * @author Vaygr
- *
+ * An abstract base class for all vertex objects
  */
 public abstract class Vertex {
-
+	
+	// At the minimum, a vertex should have a name
+	protected String vertexName = "";
+	
+	/**
+	 * Default Constructor.
+	 */
+	public Vertex(){
+		this("");
+	}
+	
+	/**
+	 * Overloaded constructor.
+	 * @param name
+	 */
+	public Vertex(String name){
+		vertexName = name;
+	}
+	
+	/**
+	 * Set the vertex name
+	 * @param str - Name of the vertex to be set.
+	 */
+	public void setVertexName(String str){
+		vertexName = str;
+	}
+	
+	/**
+	 * Get the name of the vertex
+	 * @return - Vertex Name
+	 */
+	public String getVertexName(){
+		return vertexName;
+	}
 }
