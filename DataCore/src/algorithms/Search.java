8dd32f51e3027d3f183a50f0b1597d971e7eea26
Allan Minn <allanminn@yahoo.com>
2016-08-06 17:28:05 -0700

    Added Rabin Karp's Sub-string search implementation

--- DataCore/src/algorithms/Search.java
+++ DataCore/src/algorithms/Search.java
@@ -3,6 +3,8 @@
  */
 package algorithms;
 
+import java.util.HashMap;
+
 /**
  * @author Vaygr
  *
@@ -18,7 +20,52 @@ public class Search {
 	
 	public static int rabinKarpStringSearch(String text, String pattern){
 		// TODO Implement Rabin Karp String search algorithm
-		return -1;
+		// Take the string. Creates an array of chars of string's length
+		// At each array's element location creates a rolling hash code.
+		// 		This code will cover sub-string hash code.
+		// Now, use the same hash generation on the pattern.
+		// Look up the hash table.
+		HashMap<Integer, Integer> codeMap = new HashMap<Integer, Integer>();
+		int tLength = text.length();
+		int pLength = pattern.length();
+		int prevPatternHash = 0;
+		for ( int i = 0; i < tLength - pLength; i++){
+			String nextPattern = text.substring(i, i+pLength);
+			int curPatternHash = genRollingHash(text, nextPattern, prevPatternHash, i-1, 26);
+			codeMap.put(curPatternHash,i);
+			prevPatternHash = curPatternHash;
+		}
+		int patternHash = genRollingHash(pattern, 26); //(text, pattern,codeMap, -1, 26);
+		return codeMap.get(patternHash);
+	}
+	
+	private static int genRollingHash(String pattern, int base){
+		int pHashCode = 0;
+		for ( int i= pattern.length() - 1; i >= 0; i--){
+			int charCode = pattern.codePointAt(i);
+			int exponent = (pattern.length() - 1) - i;
+			pHashCode += ( charCode * (Math.pow(base, exponent)));
+		}
+		return pHashCode;
+	}
+	
+	private static int genRollingHash(String text, String pattern, int prevPatternHash, int prevLoc, int base) {
+		// Let's assume that the string is all lower cases.
+		// Then generate base 26 number.
+		int pHashCode = 0;
+		if ( prevLoc < 0 ){ // This is the first or single pattern
+			pHashCode = genRollingHash(pattern, base);
+		}
+		else {
+			// new Code = previousHash - value of the prev char + Hash of the new char.
+			int prevCharCode = text.codePointAt(prevLoc);
+			int curCharHash = pattern.codePointAt(pattern.length() - 1);
+			int exponent = pattern.length() - 1;
+			int prevCharHash = (int)(prevCharCode * (Math.pow(base, exponent))); 
+			int curPatternHash = ((prevPatternHash - prevCharHash) * base ) + curCharHash; 	
+			pHashCode = curPatternHash;
+		}
+		return pHashCode;
 	}
 	
 	public static int binarySearch(int target, int[] input){
--- DataCore/src/testsuite/SortAndSearchTest.java
+++ DataCore/src/testsuite/SortAndSearchTest.java
@@ -18,6 +18,8 @@ import algorithms.Sort;
  */
 public class SortAndSearchTest {
 	private int[] simpleArray = null; 
+	private String inText = "";
+	private String inPattern = "";
 
 	/**
 	 * @throws java.lang.Exception
@@ -39,6 +41,8 @@ public class SortAndSearchTest {
 	@Before
 	public void setUp() throws Exception {
 		simpleArray = new int[] { 6,7,2,4,8,3,5,1,12,9,11,10 };
+		inText = "A long long time ago in a galaxy far far away.";
+		inPattern = "galaxy";
 	}
 
 	/**
@@ -49,16 +53,30 @@ public class SortAndSearchTest {
 	}
 
 	@Test
-	public void test() {
+	public void testMergeSort() {
 		System.out.println("Testing Merge Sort ... ");
 		System.out.print("Original Array : ");
 		printArray(simpleArray);
 		Sort.mergeSort(simpleArray);
 		printArray(simpleArray);
-		
-		System.out.println("Testing binary search for 10... ");
+	}
+	
+	@Test
+	public void testBinarySearch(){
+		Sort.mergeSort(simpleArray);
+		printArray(simpleArray);
+		System.out.println("\nTesting binary search for 10... ");
 		int index = Search.binarySearch(10, simpleArray);
 		System.out.println("Search target found at index: " + index);
+	}
+	
+	@Test
+	public void testRabinKarpStringSearch(){
+		System.out.println("\nTesting Rabin Karp's String Search ...");
+		System.out.println("Searching for : [ " + inPattern + " ]");
+		System.out.println("In the text : " + inText);
+		int loc = Search.rabinKarpStringSearch(inText, inPattern);
+		System.out.println("Target pattern found at : " + loc);
 	}
 	
 	private void printArray(int[] input){
